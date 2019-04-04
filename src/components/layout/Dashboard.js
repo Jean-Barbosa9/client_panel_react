d07c8e6f95bdecf43034bd7aebacffca8fb4e99f
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-04 08:16:44 -0300

    Ajuste fino

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -6,7 +6,6 @@ export default function Dashboard() {
   return (
     <div className="row">
       <div className="col-md-10">
-        
         <Clients />
       </div>
       <div className="col-md-2">
