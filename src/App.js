45833e59fff918e9010928b46a83a33bca293c2f
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-05 08:09:01 -0300

    Importando store para a aplicação

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store'
 import AppNavbar from './components/layout/AppNavbar'
 import Dashboard from './components/layout/Dashboard';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <AppNavbar />
-        <Switch>
-          <div className="container">
-            <Route exact path='/' component={Dashboard} />
-          </div>
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <AppNavbar />
+          <Switch>
+            <div className="container">
+              <Route exact path='/' component={Dashboard} />
+            </div>
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
