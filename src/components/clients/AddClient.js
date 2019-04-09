8424705807f94c2051ffdb6e9fd0df13db97cb22
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-09 08:31:15 -0300

    Criando o componente de adicionar cliente

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { firestoreConnect } from 'react-redux-firebase';
+
+class AddClient extends Component {
+  state = {
+    firtName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: ''
+  }
+
+  onChange = e => this.setState({[e.target.name]: e.target.value})
+  
+  onSubmit = e => {
+    e.preventDefault();
+    const { firestore, history } = this.props
+
+    const newClient = this.state;
+
+    if(newClient.balance === '') {
+      newClient.balance =  0
+    }
+
+    firestore.add({collection: 'clients'}, newClient).then(history.push('/'))
+  }
+  
+  render() {
+    return (
+      <div className="add-client__main-wrapper row">
+        <div className="col-md-6">
+          <div className="add-clients__back">
+            <Link to="/" className="btn btn-link">
+              <i className="icon-back fas fa-arrow-circle-left" />
+              {' '}Back to Dashboard
+            </Link>
+          </div>
+          <div className="card">
+            <div className="card-header">Add Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input 
+                    className="form-control"
+                    type="text"
+                    name="firstName"
+                    minLength="2"
+                    onChange={this.onChange}
+                    value={this.state.firstName}
+                    required="required"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input 
+                    className="form-control"
+                    type="text"
+                    name="lastName"
+                    minLength="2"
+                    onChange={this.onChange}
+                    value={this.state.lastName}
+                    required="required"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    className="form-control"
+                    type="email"
+                    name="email"
+                    onChange={this.onChange}
+                    value={this.state.email}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input 
+                    className="form-control"
+                    type="text"
+                    name="phone"
+                    onChange={this.onChange}
+                    value={this.state.phone}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input 
+                    className="form-control"
+                    type="text"
+                    name="balance"
+                    onChange={this.onChange}
+                    value={this.state.balance}
+                  />
+                </div>
+                <input type="submit" className="btn btn-block btn-primary" value="Add Client" />
+              </form>
+            </div>
+        </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+AddClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+}
+
+export default firestoreConnect()(AddClient);
