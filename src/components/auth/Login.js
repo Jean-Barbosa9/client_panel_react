1c3abac2b71d70cad8ab93055fc730893c5a93fc
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-12 15:08:15 -0300

    Inclusao do componente de login

---
+++ src/components/auth/Login.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+import { firebaseConnect } from "react-redux-firebase";
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const {firebase} = this.props;
+    const {email, password} = this.state
+
+    firebase.login({email, password}).catch(alert('Invalid login credentials!'))
+  }
+  
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-8 mx-auto">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center pb-4 pt-3">
+                <span className="text-primary">
+                  <i className="fas fa-lock"></i>
+                  Login
+                </span>
+              </h1>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholde="enter your email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholde="enter your password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    className="form-control"
+                  />
+                </div>
+                <input type="submit" value="Login" className="btn btn-primary btn-block"/>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Login.propTypes = {
+  firebase: PropTypes.object.isRequired
+}
+
+export default firebaseConnect()(Login)
