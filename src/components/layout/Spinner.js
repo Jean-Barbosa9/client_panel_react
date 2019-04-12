558d92d67d1afcd7044785f453e319f46e97e24f
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-12 07:26:57 -0300

    Centralizando spinner

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import spinner from './spinner.gif';
+import React from "react";
+import spinner from "./spinner.gif";
 
 export default function Spinner() {
   return (
-    <div>
-      <img src={spinner} alt="Loading..."/>
+    <div style={{ textAlign: "center" }}>
+      <img src={spinner} alt="Loading..." />
     </div>
-  )
+  );
 }
