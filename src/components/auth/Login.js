1233d49b6ced84e2ed6c13c14a59a04705972a31
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-04-16 22:53:25 -0300

    Importando e implementando as notificações para o login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import { compose } from "redux";
-// import { connect } from "react-redux";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import { firebaseConnect } from "react-redux-firebase";
+import { notifyUser } from "../../actions/notifyActions";
+import Alert from "../layout/Alert";
 
 class Login extends Component {
   state = {
@@ -17,21 +19,37 @@ class Login extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const { firebase } = this.props;
+    const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
-    firebase.login({ email, password }).catch(err => {
-      console.error("Error: ", err);
-      alert("Invalid login credentials!");
-    });
+    firebase
+      .login({ email, password })
+      .then(notifyUser("Checking...", "loading"))
+      .catch(error => {
+        console.error("Error: ", error);
+
+        switch (error.code) {
+          case "auth/user-not-found":
+            return notifyUser("User does not exist", "error");
+          case "auth/wrong-password":
+            return notifyUser("You entered an invalid password", "error");
+          default:
+            return notifyUser("Something went wrong", "error");
+        }
+      });
   };
 
   render() {
+    const { message, messageType } = this.props.notify;
+
     return (
       <div className="row">
         <div className="col-md-8 mx-auto">
           <div className="card">
             <div className="card-body">
+              {message ? (
+                <Alert message={message} messageType={messageType} />
+              ) : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" />
@@ -77,4 +95,12 @@ Login.propTypes = {
   firebase: PropTypes.object.isRequired
 };
 
-export default firebaseConnect()(Login);
+export default compose(
+  firebaseConnect(),
+  connect(
+    (state, props) => ({
+      notify: state.notify
+    }),
+    { notifyUser }
+  )
+)(Login);
